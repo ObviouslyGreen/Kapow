e516b1f4296130d5f0ad1bc53f5f43122a5dbd6a
Michael Chen <myc30894@gmail.com>
2015-04-15 13:35:17 -0500

    Bilateral Works, but result is meh

--- app/src/main/java/syao6_mychen5/ece420/uiuc/kapow/MainActivity.java
+++ app/src/main/java/syao6_mychen5/ece420/uiuc/kapow/MainActivity.java
@@ -1,19 +1,41 @@
 package syao6_mychen5.ece420.uiuc.kapow;
 
+/*import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import me.uits.aiphial.imaging.FastMatrixMS;
+import me.uits.aiphial.imaging.Tools;*/
+
 import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Mat;
 
 import org.opencv.android.OpenCVLoader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static org.opencv.android.Utils.matToBitmap;
+import static org.opencv.imgproc.Imgproc.bilateralFilter;
 
 public class MainActivity extends Activity
 {
@@ -57,11 +79,67 @@ public class MainActivity extends Activity
             {
                 Uri selectedImageUri = data.getData();
                 selectedImagePath = getPath(selectedImageUri);
-                displayPhoto(selectedImageUri);
+                Bitmap bitmap = null;
+                try
+                {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                Log.w("myApp", "pooooooop111111");
+                Uri filteredUri = filter(bitmap);
+      //          naiveSegmentation(selectedImagePath);
+        //        displayPhoto(Uri.parse("./msout.png"));
+                displayPhoto(filteredUri);
             }
         }
     }
 
+    public Uri filter(Bitmap bmp){
+        Mat imgMAT = new Mat(bmp.getHeight(), bmp.getWidth(), CvType.CV_8UC3);
+        Mat out = new Mat(bmp.getHeight(), bmp.getWidth(), CvType.CV_8UC3);
+        Utils.bitmapToMat(bmp, imgMAT);
+
+        //Make Mats 8-bit 3 Channel
+        Mat dst = new Mat();
+        Imgproc.cvtColor(imgMAT,dst,Imgproc.COLOR_BGRA2BGR);
+
+        bilateralFilter(dst, out, 7, 180, 180);
+
+        //Mat to Bitmap
+        Bitmap bmpout = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), Bitmap.Config.ARGB_8888);
+        matToBitmap(out, bmpout);
+
+        //Bitmap to Uri
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bmpout.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(MyApplication.getAppContext().getContentResolver(), bmpout, "Title", null);
+        return Uri.parse(path);
+    }
+
+
+
+    /*public void naiveSegmentation(String path)
+    {
+        BufferedImage srcimg = ImageIO.read(new File(path));
+        // then create a Clusterer, FastMatrixMS is a simple Mean Shift Clusterer for images
+        FastMatrixMS a = new FastMatrixMS(Tools.matrixFromImage(srcimg));
+
+        // setup filter parametrs
+        a.setColorRange(7f);
+        a.setSquareRange((short)20);
+
+        // process
+        a.doClustering();
+
+        // paint clusters on image
+        BufferedImage img = Tools.paintClusters(srcimg.getWidth(), srcimg.getHeight(), a.getClusters(), false);
+
+        // write results to file
+        ImageIO.write(img, "png", new File("./msout.png"));
+    }
+    */
     public void displayPhoto(Uri uri)
     {
         ImageView img = (ImageView)findViewById(R.id.image_input);
@@ -116,3 +194,4 @@ public class MainActivity extends Activity
         return super.onOptionsItemSelected(item);
     }
 }
+
