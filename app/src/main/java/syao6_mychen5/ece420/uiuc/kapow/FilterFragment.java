57747de11c1696bcd5215dff15d2385ff1100820
Michael Chen <myc30894@gmail.com>
2015-04-27 21:56:53 -0500

    filter work. added gpuimage lib

--- app/src/main/java/syao6_mychen5/ece420/uiuc/kapow/FilterFragment.java
+++ app/src/main/java/syao6_mychen5/ece420/uiuc/kapow/FilterFragment.java
@@ -3,15 +3,38 @@ package syao6_mychen5.ece420.uiuc.kapow;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.TermCriteria;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import syao6_mychen5.ece420.uiuc.kapow.GPUImage.GPUImage;
+import syao6_mychen5.ece420.uiuc.kapow.GPUImage.GPUImageToonFilter;
+
+import static org.opencv.android.Utils.matToBitmap;
+import static org.opencv.imgproc.Imgproc.pyrMeanShiftFiltering;
+
 
 
 /**
@@ -157,9 +180,104 @@ public class FilterFragment extends Fragment
             if (requestCode == SELECT_PICTURE)
             {
                 Uri selectedImageUri = data.getData();
-                displayPhoto(selectedImageUri);
+                Bitmap bitmap = null;
+                try
+                {
+                    bitmap = MediaStore.Images.Media.getBitmap(MyApplication.getAppContext().getContentResolver(), selectedImageUri);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                Log.w("myApp", "pooooooop111111");
+                Uri filteredUri = null;
+                try
+                {
+                    filteredUri = filter(bitmap);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                displayPhoto(filteredUri);
             }
         }
+    }
+
+    public Uri filter(Bitmap bmp) throws IOException
+    {
+        Mat imgMAT = new Mat(bmp.getHeight(), bmp.getWidth(), CvType.CV_8UC3);
+        Mat out = new Mat(bmp.getHeight(), bmp.getWidth(), CvType.CV_8UC3);
+        Utils.bitmapToMat(bmp, imgMAT);
+
+        //Make Mats 8-bit 3 Channel
+        Mat temp = new Mat();
+        Imgproc.cvtColor(imgMAT,temp,Imgproc.COLOR_BGRA2BGR);
+        Mat dst = new Mat();
+        Imgproc.cvtColor(temp,dst,Imgproc.COLOR_BGR2Luv);
+
+
+        //bilateralFilter(dst, out, 7, 180, 180);
+        TermCriteria termcrit = new TermCriteria(TermCriteria.COUNT + TermCriteria.EPS, 5, 1);
+        pyrMeanShiftFiltering(dst, out, 30, 30, 2, termcrit);
+
+      /*  Scalar zero = Scalar.all(0);
+        Scalar colorDiff = Scalar.all(1);
+        Mat mask = new Mat( out.rows()+2, out.cols()+2, CvType.CV_8UC1, zero);
+        for( int y = 0; y < out.rows(); y++ )
+            {
+            for( int x = 0; x < out.cols(); x++ )
+            {
+                byte buff[] = new byte[(int) (mask.total() * mask.channels())];
+                mask.get(x+1, y+1, buff);
+                if(0 == buff[0])
+                {
+                    Scalar newVal = new Scalar( random()%256, random()%256, random()%256 );
+                    Point pt = new Point(x,y);
+                    Rect test = new Rect();
+                    floodFill( out, mask, pt, newVal, test, colorDiff, colorDiff, 4);
+                }
+            }
+        }*/
+
+        //Mat to Bitmap
+        Mat newout = new Mat(bmp.getHeight(), bmp.getWidth(), CvType.CV_8UC3);;
+        Imgproc.cvtColor(out,newout,Imgproc.COLOR_Luv2BGR);
+        Bitmap bmpoutMS = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), Bitmap.Config.ARGB_8888);
+        matToBitmap(newout, bmpoutMS);
+
+        GPUImage mGPUImage = new GPUImage(MyApplication.getAppContext());
+        mGPUImage.setFilter(new GPUImageToonFilter());
+        mGPUImage.setImage(bmpoutMS);
+        Bitmap bmpout = mGPUImage.getBitmapWithFilterApplied();
+
+        //Bitmap to Uri
+        int counter = 0;
+        OutputStream fOut = null;
+        File file; // the File to save to
+        String path = null;
+        File sd = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File directory = new File(sd, "Kapow");
+        try {
+            directory.mkdirs();
+        } catch(Exception e) {}
+        file = new File(sd, "Kapow/"+"output.png");
+        while (file.exists()) {
+            counter++;
+            file = new File(sd, "Kapow/" + "output" + "(" + counter + ").png");
+        }
+        try{
+            fOut = new FileOutputStream(file);
+            bmpout.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+            path = MediaStore.Images.Media.insertImage(MyApplication.getAppContext().getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            Toast.makeText(MyApplication.getAppContext(), "Save Failed", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        return Uri.parse(path);
     }
 
     public void displayPhoto(Uri uri)
